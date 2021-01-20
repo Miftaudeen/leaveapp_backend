527e5f44d1cae95bed75be510911787de3c8873f
Miftaudeen <damilarelamidi@gmail.com>
2021-01-20 23:44:25 +0100

    First Commit

---
+++ leaveapp_backend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for leaveapp_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leaveapp_backend.settings')
+
+application = get_asgi_application()
