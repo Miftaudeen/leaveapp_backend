e1bb246c72ab0d5442485bcf5e065726e71cf3d4
Miftaudeen <damilarelamidi@gmail.com>
2021-01-23 10:05:24 +0100

    Removed unused import

--- employees/serializers.py
+++ employees/serializers.py
@@ -1,10 +1,7 @@
-from datetime import datetime
 
 from rest_framework import serializers
-from rest_framework_simplejwt.state import User
 
 from employees.models import Employee
-from leave.models import Leave, LeaveType, LeavePolicy, EmployeeLeaveStructure, get_number_working_days
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
